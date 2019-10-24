896ade38f429c4651d0cbe2339655b173c12318e
hao6 <1260744139@qq.com>
2019-10-24 11:02:35 +0800

    completed part(A) of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+package exercise;
+
+/**
+ * @ClassName HelloNumbers
+ * @Description TODO
+ * @Author hao6
+ * @Data 10/24/19 10:58 AM
+ * @Version 1.0
+ **/
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int i = 0 ;
+        while (i < 10) {
+            System.out.printf(i + " ");
+            i++;
+        }
+    }
+}
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * @Author hao6
+ * @Data 10/24/19 10:57 AM
+ * @Version 1.0
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
